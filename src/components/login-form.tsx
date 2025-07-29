bf54d17d8b825f90e840f6906d7acf4f56faed49
iamashwiner <ashwinidev214@gmail.com>
2025-07-29 18:29:22 +0530

    fix: fixed the issue of build

--- src/components/login-form.tsx
+++ src/components/login-form.tsx
@@ -9,7 +9,7 @@ import {
 } from "@/components/ui/card"
 import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
-import { Link } from "next/link"
+
 export function LoginForm({
   className,
   ...props
--- src/components/register-form.tsx
+++ src/components/register-form.tsx
@@ -9,7 +9,6 @@ import {
 } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "next/link";
 
 export function RegisterForm({
   className,
