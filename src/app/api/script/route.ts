6362f72b87cc1e212e892f2bdc0f3b6f7a737990
iamashwiner <ashwinidev214@gmail.com>
2025-08-01 12:12:16 +0530

    fixed the issue of build in route.ts

--- src/app/api/script/route.ts
+++ src/app/api/script/route.ts
@@ -1,7 +1,7 @@
 import prisma from "../../../../lib/prisma";
 import { NextResponse } from "next/server";
 
-export async function GET({ userId }: { userId: string }) {
+export async function GET({ userId }) {
   try {
     console.log(userId)
     const scripts = await prisma.generatedScript.findMany({
